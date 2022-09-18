e4cdd40d9e4baaffc4af1330a855e58f74d6327e
rajasekhar1404 <mrrajasekhar09@gmail.com>
2022-09-18 15:26:54 +0530

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+function Main(innertext, source) {
+    var numbers = innertext.length;
+    var div = 360 / numbers;
+    var radius = 150;
+    var parentdiv = document.getElementById('parentdiv');
+    var offsetToParentCenter = parseInt(parentdiv.offsetWidth / 2); //assumes parent is square
+    var offsetToChildCenter = 50;
+    var totalOffset = offsetToParentCenter - offsetToChildCenter;
+    for (var i = 0; i <= numbers-1; ++i) {
+      var childdiv = document.createElement('div');
+      childdiv.className = 'div2';
+      var pagename = location.pathname.substring(location.pathname.lastIndexOf("/") + 1); // for html file name
+      if(pagename == "index.html") {
+        childdiv.innerHTML = '<a href='+source[i]+'>'+"<p>"+innertext[i]+"</p>"+'</a>';        
+      } else {
+        childdiv.innerHTML = '<a href='+source[i]+' target="_blank">'+"<p>"+innertext[i]+"</p>"+'</a>';
+      }
+      childdiv.style.position = 'absolute';
+      childdiv.style.display = 'flex';
+      childdiv.style.justifyContent = 'center';
+      childdiv.style.alignItems = 'center';
+      var y = Math.sin((div * i) * (Math.PI / 180)) * radius;
+      var x = Math.cos((div * i) * (Math.PI / 180)) * radius;
+      childdiv.style.top = (y + totalOffset).toString() + "px";
+      childdiv.style.left = (x + totalOffset).toString() + "px";
+      parentdiv.appendChild(childdiv);
+    }    
+}
